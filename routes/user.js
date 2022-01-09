85efc817f06038e45001b43c5dcb37e4855744a4
PeinZero <mahadzx@gmail.com>
2022-01-10 01:42:55 +0500

    changes

--- routes/user.js
+++ routes/user.js
@@ -13,14 +13,8 @@ router.put('/switchRole', isAuth, switchRole);
 // Gets
 
 // Dev APIs
-<<<<<<< HEAD
-router.post('/deleteSpot', isAuth, deleteSpot);
-router.get('/getUser/:userId', isAuth, getUser);
-
-=======
 router.get('/all', getAllUsers);
 router.get('/getUser/:userId', isAuth, getUser);
->>>>>>> 309fae4c4984d3b278ccd5900e2601b9832ce390
 router.get("/getAllusers", getAllUsers);
 
 export default router;
