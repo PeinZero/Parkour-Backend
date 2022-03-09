4b7cdfbfca6f6af2774495add5a0251aeef172fc
PeinZero <mahadzx@gmail.com>
2022-03-09 23:26:57 +0500

    changes

--- controllers/spots.js
+++ controllers/spots.js
@@ -151,10 +151,6 @@ export let editSpot = async (req, res, next) => {
 export let requestSpot = async (req, res, next) => {
   const userId = req.userId;
   const spotId = req.params.spotId;
-  const slot = 
-  
-  try {
-
 };
 
 export let getAllSpotsBySeller = async (req, res, next) => {
